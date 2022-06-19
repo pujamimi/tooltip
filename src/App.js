97deae104deece617a1c2b8d801c2a499e7a1764
Puja Banerjee <pujab1160@gmail.com>
2022-06-19 19:58:14 +0530

    file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; 
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tippy content = "It's me, mamburi.">
+           <button className='bttn'>Click me</button>
+      </Tippy>
     </div>
   );
 }
